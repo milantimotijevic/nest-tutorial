cbf0cbb58a0c54f40374de936379c2925a876973
Milan Timotijevic <mtimo101289@gmail.com>
2024-02-22 18:39:38 +0100

    fine tune auth middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ReservationsModule } from './reservations/reservations.module';
 import { HealthModule } from './health/health.module';
 import { AuthMiddleware } from './global-middleware/auth/auth.middleware';
+import { ReservationsController } from './reservations/controllers/reservations/reservations.controller';
 
 @Module({
   imports: [ReservationsModule, HealthModule],
@@ -10,6 +11,6 @@ import { AuthMiddleware } from './global-middleware/auth/auth.middleware';
 })
 export class AppModule implements NestModule {
     configure(consumer: MiddlewareConsumer) {
-        consumer.apply(AuthMiddleware).forRoutes('');
+        consumer.apply(AuthMiddleware).forRoutes(ReservationsController);
     }
 }
